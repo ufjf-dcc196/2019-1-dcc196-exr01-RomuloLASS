33290e7b7c5a9955614ec5bdd81708098ad007f3
Romulo Soares <37163123+RomuloLASS@users.noreply.github.com>
2019-03-27 10:03:07 -0300

    Segundo commit

--- app/src/main/java/br/ufjf/exercicio001/MainActivity.java
+++ app/src/main/java/br/ufjf/exercicio001/MainActivity.java
@@ -23,7 +23,7 @@ public class MainActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                textView.setText(editText.getText()+""+editText2.getText());
+                textView.setText(editText.getText()+" "+editText2.getText());
             }
         });
     }
