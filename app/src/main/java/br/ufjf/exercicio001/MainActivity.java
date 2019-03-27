66a9e97bd88a0670e3f6c58828eec6367b080a48
Romulo Soares <37163123+RomuloLASS@users.noreply.github.com>
2019-03-27 09:53:05 -0300

    Primeiro commit

---
+++ app/src/main/java/br/ufjf/exercicio001/MainActivity.java
@@ -0,0 +1,30 @@
+package br.ufjf.exercicio001;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final TextView textView = findViewById(R.id.textView);
+
+        final EditText editText = findViewById(R.id.editText);
+        final EditText editText2 = findViewById(R.id.editText2);
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                textView.setText(editText.getText()+""+editText2.getText());
+            }
+        });
+    }
+}
